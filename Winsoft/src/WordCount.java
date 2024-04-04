12cfdcd8fbb0889641c3bc09eb35201e2decbc79
Ritesh Taru <114311680+Riteshtaru343@users.noreply.github.com>
2024-04-04 14:48:40 +0530

    Update WordCount.java

--- Winsoft/src/WordCount.java
+++ Winsoft/src/WordCount.java
@@ -4,13 +4,10 @@ public class WordCount {
     public static void main(String[] args) {
         String str = "This is a sample string with some sample words";
 
-        // Split the string into words
         String[] words = str.split(" ");
 
-        // Create a HashMap to store word frequencies
         HashMap<String, Integer> wordCountMap = new HashMap<>();
 
-        // Iterate through each word and update the word count in the HashMap
         for (String word : words) {
             if (wordCountMap.containsKey(word)) {
                 // If the word already exists in the map, increment its count
@@ -20,8 +17,6 @@ public class WordCount {
                 wordCountMap.put(word, 1);
             }
         }
-
-        // Print the word count for each word
         System.out.println("Word Count:");
         for (String word : wordCountMap.keySet()) {
             System.out.println(word + ": " + wordCountMap.get(word));
